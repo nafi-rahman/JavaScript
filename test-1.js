bd986b775383db3d4baadc7bea2f724863fd9b26
nafi-rahman <nafirahman16@gmail.com>
2023-05-06 20:22:08 +0600

    Update test-1.js

--- test-1.js
+++ test-1.js
@@ -8,7 +8,7 @@ const c = false //constant variable that can not be changed afterwards.
 
 //------------------------------------------------Objects----------------------------------------------------------
 // Objects are used to store key-value pairs. They are declared using curly braces and can contain any type of data. Here is an example:
-let person = {
+const person = {
     hand:2,
     head:1,
     tail:"none",
@@ -22,6 +22,30 @@ console.log(person["height"])
 for(let prop in person) {
   console.log(prop +":"+ person[prop])
 }
+
+
+const animal = {
+  dna:"abc123",
+  legs:{
+    front:2,
+    back:3
+  },
+  sleep () {
+    console.log("zzz")
+  },
+}
+
+const dog = {
+  bark() {
+    console.log("woof")
+  }
+}
+
+Object.setPrototypeOf(dog,animal)
+
+console.log(dog.legs)
+
+
 
 //----------------------------------------------Functions-----------------------------------------------------------
 // Functions are blocks of code that perform a specific task. They can be declared using the function keyword. 
@@ -44,3 +68,63 @@ if (age < 18) {
   console.log("You can vote and drink alcohol.");
 }
 
+
+//----------------------------------------------Destructure-----------------------------------------------------------
+const cat = {
+  name: "puffi",
+  type: "fatcat",
+  speak () {
+    console.log("meow")
+  }
+}
+
+const { name: pname, type:ptype} = cat
+
+console.log(pname)
+
+const arr = ["apple","share","bear"]
+
+const [fruit,verb,mamal] = arr
+
+console.log(mamal)
+
+//----------------------------------------------spread-----------------------------------------------------------
+const obj1 = {
+  first: 1,
+  third: 4
+}
+const obj2 = {
+  // ...obj1,
+  second: 2,
+  third:3,
+  ...obj1
+}
+
+// const merge = {...obj1,...obj2}
+console.log(obj2)
+
+//----------------------------------------------nullish-----------------------------------------------------------
+
+let x = ''
+let y = x ?? "undefined" 
+console.log(y)
+
+//----------------------------------------------Array-----------------------------------------------------------
+
+const testarr = [...Array(10).keys()]
+let sum = 0;
+for (num in testarr) {
+  sum = sum + +num
+}
+console.log(sum)
+
+
+const addArr = (arr) => {
+  for (num in arr) {
+    return sum = sum + +num
+  }
+}
+
+console.log(addArr(testarr))
+
+console.log(testarr.reduce((acc,cur) => acc + cur, 0 ))
